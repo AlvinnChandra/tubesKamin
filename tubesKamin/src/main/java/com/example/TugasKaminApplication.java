4575c6a4f1b305ebab56b8ac80c32bf34ad571b3
Alvin <192010003@smatalenta.sch.id>
2024-12-01 13:16:21 +0700

    Pembuatan UI/UX Menu

---
+++ tubesKamin/src/main/java/com/example/TugasKaminApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TugasKaminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TugasKaminApplication.class, args);
+	}
+
+}
