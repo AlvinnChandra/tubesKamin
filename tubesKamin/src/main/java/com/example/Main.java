88cbb36c3e42391554dcfbbafb11cc190db31f2c
RengganaKurniawan <6182201071@student.unpar.ac.id>
2024-12-01 15:40:36 +0700

    add decrypt

--- tubesKamin/src/main/java/com/example/Main.java
+++ tubesKamin/src/main/java/com/example/Main.java
@@ -6,6 +6,7 @@ import com.google.zxing.*;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 
+import java.io.FileOutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -16,14 +17,20 @@ import java.time.Instant;
 public class Main {
     public static void main(String[] args) {
         try {
-            String orderNumber = "102";
-            String orderDetails = "Ayam Goreng";
+            //Bikin String
+            String orderNumber = "102"; //Connect DB
+            String orderDetails = "Ayam Goreng"; //Connect DB
             String qrCodeString = generateQRCodeString(orderNumber, orderDetails);
 
-            KeyPair keyPair = generateKeyPair();
+            //Encrypt String
+            KeyPair keyPair = generateKeyPair(); //Bikin Key
+            saveKeyToFile("public.key", keyPair.getPublic());
+            saveKeyToFile("private.key", keyPair.getPrivate());
+
+
             String encryptedQRCode = encrypt(qrCodeString, keyPair.getPublic());
 
-            Date expiryTime = getExpiryTime(1);
+            Date expiryTime = getExpiryTime(60);
 
             String filePath = "src/main/resources/static/QRCode/QR102.png";
             generateQRCode(encryptedQRCode, filePath);
@@ -39,34 +46,42 @@ public class Main {
     public static String generateQRCodeString(String orderNumber, String orderDetails){
         String firstDigit = orderNumber.substring(0, 1);
         String lastTwoDigit = orderNumber.substring(orderNumber.length() - 2);
-        String formattedDetails = orderDetails.replaceAll("\\s", "");
+        String formattedDetails = orderDetails.replaceAll("\\s", ""); //Hapus Spasi
         return firstDigit + formattedDetails + lastTwoDigit;
     }
 
+    //Membuat public key dan private key
     public static KeyPair generateKeyPair() throws NoSuchAlgorithmException {
-        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
-        keyGen.initialize(2048);
-        return keyGen.generateKeyPair();
+        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA"); //Membuat instance key generator yang menggunakan algoritma RSA
+        keyGen.initialize(2048); //Besar ukuran kunci
+        return keyGen.generateKeyPair(); //Menghasilkan public key dan private key
     }
 
     public static String encrypt(String data, PublicKey publicKey) throws Exception {
-        Cipher cipher = Cipher.getInstance("RSA");
-        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
-        byte[] encryptedData = cipher.doFinal(data.getBytes());
-        return Base64.getEncoder().encodeToString(encryptedData);
+        Cipher cipher = Cipher.getInstance("RSA"); //Object Cipher untuk enkripsi dengan algoritma RSA
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey); //Encrypt menggunakan public Key
+        byte[] encryptedData = cipher.doFinal(data.getBytes()); //Konversi String menjadi byte agar dapat dienkripsi
+        return Base64.getEncoder().encodeToString(encryptedData); //mengubah kembali ke string
     }
 
+    //Waktu expire QR
     public static Date getExpiryTime(int minutes){
         return Date.from(Instant.now().plusSeconds(minutes * 60));
     }
 
     public static void generateQRCode(String data, String filePath) throws Exception {
-        Path path = Paths.get(filePath);
+        Path path = Paths.get(filePath); //Path qr code disimpan
         if(!Files.exists(path.getParent())){
-            Files.createDirectories(path.getParent());
+            Files.createDirectories(path.getParent()); //Cek file
         }
 
-        BitMatrix matrix = new MultiFormatWriter().encode(data, BarcodeFormat.QR_CODE, 200, 200);
-        MatrixToImageWriter.writeToPath(matrix, "PNG", path);
+        BitMatrix matrix = new MultiFormatWriter().encode(data, BarcodeFormat.QR_CODE, 200, 200); //membuat QR Code
+        MatrixToImageWriter.writeToPath(matrix, "PNG", path); //BitMatrix diubah menjadi png
+    }
+
+    public static void saveKeyToFile(String filename, Key key) throws Exception {
+        try(FileOutputStream fos = new FileOutputStream(filename)){
+            fos.write(key.getEncoded());
+        }
     }
 }
---
+++ tubesKamin/src/main/java/com/example/QRCodeScanner.java
@@ -0,0 +1,66 @@
+package com.example;
+
+import com.google.zxing.*;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.common.HybridBinarizer;
+
+import javax.crypto.Cipher;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.KeyFactory;
+import java.security.PrivateKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.util.Base64;
+import javax.imageio.ImageIO;
+
+public class QRCodeScanner {
+    public static void main(String[] args) {
+        try {
+            //Path QR Code
+            String filePath = "src/main/resources/static/QRCode/QR102.png"; //File QR yang mau dibaca
+
+            PrivateKey privateKey = loadPrivateKey("private.key");
+
+            //Read QR Code
+            String encryptedQRCode = readQRCode(filePath); //Baca QR dari path
+
+            String decryptedData = decrypt(encryptedQRCode, privateKey);
+            System.out.println("Decrypted QR Code Data: " + decryptedData); //String yang didecrypt
+
+            String orderNumber = decryptedData.substring(0, 1) + decryptedData.substring(decryptedData.length() - 2);
+            String orderDetails = decryptedData.substring(1, decryptedData.length() - 2);
+            System.out.println("Order Number: " + orderNumber);
+            System.out.println("Order Details: " + orderDetails);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public static String readQRCode(String filePath) throws Exception {
+        Path path = new File(filePath).toPath(); //ambil path QR Code
+        BufferedImage image = ImageIO.read(path.toFile()); //Baca gambar QR COde
+        LuminanceSource source = new BufferedImageLuminanceSource(image); //Scan QR Code
+        BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source)); //Buat bitMap
+
+        Result result = new MultiFormatReader().decode(bitmap); //Baca data dari bitmap
+        return result.getText();
+    }
+
+    public static String decrypt(String encryptedData, PrivateKey privateKey) throws Exception{
+        Cipher cipher = Cipher.getInstance("RSA"); //CIpher gunakan algoritma RSA
+        cipher.init(Cipher.DECRYPT_MODE, privateKey); //Decrypt data
+        byte[] decryptedData = cipher.doFinal(Base64.getDecoder().decode(encryptedData)); //Hasil enkripsi ke array byte
+        return new String(decryptedData); //array byte to string
+    }
+
+    public static PrivateKey loadPrivateKey(String filename) throws Exception{
+        byte[] keyBytes = Files.readAllBytes(Paths.get(filename)); //Baca file private.key
+        PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(keyBytes); //Mengubah format ke PKCS#8
+        KeyFactory kf = KeyFactory.getInstance("RSA"); 
+        return kf.generatePrivate(spec); //Menghasilkan privateKey dari file
+
+    }
+}
