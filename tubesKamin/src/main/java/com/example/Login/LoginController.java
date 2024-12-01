539e9c32a8ce672e032c9e1624db4248ae2b1c7b
Alvin <192010003@smatalenta.sch.id>
2024-12-01 14:57:47 +0700

    Sudah diintegrasikan dengan login

---
+++ tubesKamin/src/main/java/com/example/Login/LoginController.java
@@ -0,0 +1,51 @@
+package com.example.Login;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
+
+@Controller
+public class LoginController {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    // Menampilkan halaman login
+    @GetMapping("/login")
+    public String showLoginForm() {
+        return "/Restoran/login"; // Mengarah ke login.html
+    }
+
+    // Menangani proses login
+    @PostMapping("/login")
+    public String login(@RequestParam String username, @RequestParam String password, Model model) {
+        String sql = "SELECT * FROM users WHERE username = ? AND password = ?";
+        try {
+            Map<String, Object> user = jdbcTemplate.queryForMap(sql, username, password);
+
+            String role = (String) user.get("role");
+
+            if ("admin".equals(role)) {
+                return "redirect:/menu";
+            } else {
+                model.addAttribute("error", "Role tidak ditemukan");
+            }
+        } catch (Exception e) {
+
+            model.addAttribute("error", "Nama pengguna atau kata sandi tidak valid");
+        }
+        return "login";
+    }
+
+    // Halaman menu
+    @GetMapping("/menu")
+    public String menuPage() {
+        return "/Restoran/menu";
+    }
+}
