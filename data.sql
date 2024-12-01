a0e5b79703af6a8c16fe026de4f2c2380188d8cd
Alvin <192010003@smatalenta.sch.id>
2024-12-01 15:56:58 +0700

    Update Data

--- data.sql
+++ data.sql
@@ -8,11 +8,11 @@ CREATE TABLE users (
 INSERT INTO users (username, password, role) VALUES ('admin', 'admin123', 'admin');
 
 
-CREATE TABLE users (
+CREATE TABLE orders (
     id SERIAL PRIMARY KEY,
-    username VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    role VARCHAR(50) NOT NULL
+	menu VARCHAR(100) NOT NULL,
+ 	jumlah INT NOT NULL,
+	nama VARCHAR(255) NOT NULL,
+    phone VARCHAR(20) NOT NULL,
+    order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
-INSERT INTO users (username, password, role) VALUES ('admin', 'admin123', 'admin');
