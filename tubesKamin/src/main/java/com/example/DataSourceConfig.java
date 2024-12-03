d3e1f81a1d157b8a7b81ff3e5402e164babfa1ea
zefandion <zefandion03@gmail.com>
2024-12-03 18:43:46 +0700

    penambahan style menu + gabung branch juan

--- tubesKamin/src/main/java/com/example/DataSourceConfig.java
+++ tubesKamin/src/main/java/com/example/DataSourceConfig.java
@@ -23,7 +23,7 @@ public class DataSourceConfig {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setUrl(url);
         dataSource.setUsername("postgres");
-        dataSource.setPassword("postgres");
+        dataSource.setPassword("agpd2107");
         return dataSource;
     }
 }
